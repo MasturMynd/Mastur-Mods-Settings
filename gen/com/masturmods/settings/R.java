8c767c7d47394b63ec12b987cf99898c0f876a28
Andrew Speers <masturmynd@live.com>
2012-01-24 11:20:43 -0500

    Complete clean up. Support section now completely functional

--- gen/com/masturmods/settings/R.java
+++ gen/com/masturmods/settings/R.java
@@ -20,59 +20,68 @@ public final class R {
     }
     public static final class attr {
     }
-    public static final class color {
-        public static final int white=0x7f050000;
-    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button=0x7f020000;
+        public static final int button_focused=0x7f020001;
+        public static final int button_normal=0x7f020002;
+        public static final int button_pressed=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int Support=0x7f060017;
-        public static final int back_button=0x7f060004;
-        public static final int back_chooser=0x7f060005;
-        public static final int battery=0x7f06000f;
-        public static final int battery_chooser=0x7f060008;
-        public static final int bttery_chooser=0x7f060010;
-        public static final int color_white=0x7f060001;
-        public static final int data_chooser=0x7f060009;
-        public static final int data_icon=0x7f060011;
-        public static final int donate=0x7f06000d;
-        public static final int email=0x7f06000c;
-        public static final int home_button=0x7f060015;
-        public static final int home_chooser=0x7f060006;
-        public static final int nav_bar=0x7f060014;
-        public static final int recent_button=0x7f060016;
-        public static final int recent_chooser=0x7f060007;
-        public static final int settings_pager=0x7f060000;
-        public static final int signal_chooser=0x7f06000a;
-        public static final int signal_icon=0x7f060012;
-        public static final int stat_bar_title=0x7f06000e;
-        public static final int status_bar=0x7f060002;
-        public static final int status_bar_title=0x7f060003;
-        public static final int webView1=0x7f060018;
-        public static final int webView2=0x7f060019;
-        public static final int wifi_chooser=0x7f06000b;
-        public static final int wifi_icon=0x7f060013;
+        public static final int Support=0x7f050019;
+        public static final int back_button=0x7f050004;
+        public static final int back_chooser=0x7f050005;
+        public static final int battery=0x7f050011;
+        public static final int battery_chooser=0x7f050008;
+        public static final int bttery_chooser=0x7f050012;
+        public static final int button_donate=0x7f05000f;
+        public static final int button_email=0x7f05000d;
+        public static final int color_white=0x7f050001;
+        public static final int data_chooser=0x7f050009;
+        public static final int data_icon=0x7f050013;
+        public static final int donate=0x7f05000e;
+        public static final int email=0x7f05000c;
+        public static final int home_button=0x7f050017;
+        public static final int home_chooser=0x7f050006;
+        public static final int nav_bar=0x7f050016;
+        public static final int recent_button=0x7f050018;
+        public static final int recent_chooser=0x7f050007;
+        public static final int settings_pager=0x7f050000;
+        public static final int signal_chooser=0x7f05000a;
+        public static final int signal_icon=0x7f050014;
+        public static final int stat_bar_title=0x7f050010;
+        public static final int status_bar=0x7f050002;
+        public static final int status_bar_title=0x7f050003;
+        public static final int wifi_chooser=0x7f05000b;
+        public static final int wifi_icon=0x7f050015;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f070000;
-        public static final int back_button=0x7f070007;
-        public static final int battery=0x7f07000a;
-        public static final int data_icon=0x7f07000b;
-        public static final int donate=0x7f07000f;
-        public static final int email=0x7f07000e;
-        public static final int home_button=0x7f070008;
-        public static final int nav_bar_title=0x7f070005;
-        public static final int navigation_bar=0x7f070002;
-        public static final int recent_button=0x7f070009;
-        public static final int signal_icon=0x7f07000c;
-        public static final int stat_bar_title=0x7f070004;
-        public static final int status_bar=0x7f070001;
-        public static final int support=0x7f070003;
-        public static final int support_title=0x7f070006;
-        public static final int wifi_icon=0x7f07000d;
+        public static final int app_name=0x7f060000;
+        public static final int back_button=0x7f060009;
+        public static final int battery=0x7f060003;
+        public static final int data_icon=0x7f060004;
+        public static final int donate=0x7f06000f;
+        public static final int email=0x7f06000e;
+        public static final int home_button=0x7f06000a;
+        public static final int nav_bar_title=0x7f060008;
+        /**  End Status Bar Strings 
+Begin Navigation Strings 
+         */
+        public static final int navigation_bar=0x7f060007;
+        public static final int recent_button=0x7f06000b;
+        public static final int signal_icon=0x7f060005;
+        public static final int stat_bar_title=0x7f060002;
+        /** Begin Status Bar Strings 
+         */
+        public static final int status_bar=0x7f060001;
+        /**  End Navigation Strings 
+Begin Support Strings 
+         */
+        public static final int support=0x7f06000c;
+        public static final int support_title=0x7f06000d;
+        public static final int wifi_icon=0x7f060006;
     }
 }
--- src/com/masturmods/settings/MasturModsSettingsActivity.java
+++ src/com/masturmods/settings/MasturModsSettingsActivity.java
@@ -1,13 +1,58 @@
 package com.masturmods.settings;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.os.Bundle;
+import com.masturmods.settings.R;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MasturModsSettingsActivity extends Activity {
+	String url = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=7NN6B7L7YXQ82&lc=US&item_name=Mastur%20Mods&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted";
+    Intent i = new Intent(Intent.ACTION_VIEW);
+    Uri u = Uri.parse(url);
+    Context context = this;
+
+    private Button button_email;
+	private Button button_donate;
+
     /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-}
+		@Override
+    	public void onCreate(Bundle savedInstanceState) {
+    		super.onCreate(savedInstanceState);
+    		setContentView(R.layout.main);
+        
+    		button_donate = (Button)findViewById(R.id.button_donate);
+    		button_donate.setOnClickListener(new OnClickListener() {         
+    			@Override
+    			public void onClick(View v){
+    				try {
+    					i.setData(u);
+    					startActivity(i);
+    				} catch (ActivityNotFoundException e) {
+    					Toast.makeText(context, "Browser not found.", Toast.LENGTH_SHORT);
+    				}
+    			} 
+    		});
+	
+    		final Intent emailIntent = new Intent( android.content.Intent.ACTION_SEND);
+    		
+    		button_email = (Button)findViewById(R.id.button_email);
+    		button_email.setOnClickListener(new OnClickListener() {
+    			@Override
+    			public void onClick(View v){
+    				emailIntent.setType("message/rfc822");
+    				emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[] { "masturmynd@gmail.com" });
+    				emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Mastur Mods Settings");
+    				emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "");
+    				startActivity(emailIntent);
+    			}
+    		});
+		}
+	}
